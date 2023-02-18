3550e9bb5b55322ac0663400f1e114741d089666
Etherealsdu <zhangpsdu2009@gmail.com>
2023-02-18 22:34:57 +0800

    lecture 1: single OSD

---
+++ objects/put.go
@@ -0,0 +1,22 @@
+package objects
+
+import (
+	"distributeOSD/util"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func put(w http.ResponseWriter, r *http.Request) {
+	f, e := os.Create(util.Pconfig.StorageRoot + "/objects/" +
+		strings.Split(r.URL.EscapedPath(), "/")[2])
+	if e != nil {
+		log.Println(e)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, r.Body)
+}
